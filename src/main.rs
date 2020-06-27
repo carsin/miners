b335541fc79bb85770d28331d2f53f6c8ba7a339
carsin <carsin@users.noreply.github.com>
2020-06-26 21:22:36 -1000

    attempt game loop refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate crossterm;
 
 const TICKS_PER_SECOND: u64 = 1;
-const TICKS_TO_SKIP: u64 = 1000 / TICKS_PER_SECOND;
+const TICKS_TO_SKIP: u64 = 1000000000 / TICKS_PER_SECOND;
 const MAX_FRAMESKIP: i64 = 10; // Max number of frames before rendering. Game rendering to drop to this speed before updating slows.
 
 use crossterm::{cursor, terminal, ExecutableCommand, QueueableCommand, style::Print};
 use std::io::stdout;
 use std::thread::sleep;
-use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
+use std::time::{Duration, Instant};
 
 fn main() {
     start();
@@ -15,29 +15,46 @@ fn main() {
 
 fn run() {
     let start_time = Instant::now();
-    let mut tick_count: u128 = start_time.elapsed().as_millis();
-    let mut loops: i64;
+    let mut next_time: u64 = start_time.elapsed().as_nanos() as u64;
+    let max_time_diff: u64 = 500;
+
+    let mut skipped_frames = 1;
+    let max_skipped_frames = 5;
 
     let mut update_count = 0;
     let mut render_count = 0;
 
     let running = true;
     while running {
-        loops = 0;
+        let mut curr_time: u64 = start_time.elapsed().as_nanos() as u64;
 
-        while start_time.elapsed().as_millis() > tick_count && loops < MAX_FRAMESKIP {
+        if (curr_time - next_time) > max_time_diff {
+            next_time = curr_time;
+        }
+
+        if curr_time >= next_time {
+            next_time += TICKS_TO_SKIP;
             // Update
             update_count += 1;
 
-            tick_count += TICKS_TO_SKIP as u128;
-            loops += 1;
+            if curr_time < next_time || skipped_frames > max_skipped_frames {
+                // Render
+                render_count += 1;
+                stdout().queue(cursor::MoveTo(0, 0)).unwrap()
+                        .queue(Print(format!("Updates: {}", update_count))).unwrap();
+                stdout().queue(cursor::MoveTo(0, 1)).unwrap()
+                        .queue(Print(format!("Renders: {}", render_count))).unwrap();
+
+                skipped_frames = 1
+            } else {
+                skipped_frames += 1;
+            }
+        } else {
+            let sleep_time = next_time - curr_time;
+            if sleep_time > 0 {
+                sleep(Duration::from_nanos(sleep_time));
+            }
         }
-        // Render
-        render_count += 1;
-        stdout().queue(cursor::MoveTo(0, 0)).unwrap()
-                .queue(Print(format!("Updates: {}", update_count))).unwrap();
-        stdout().queue(cursor::MoveTo(0, 1)).unwrap()
-                .queue(Print(format!("Renders: {}", render_count))).unwrap();
     }
     stop(); // Stop after loop breaks
 }
