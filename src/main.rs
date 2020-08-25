301637b74cb96425e62802386356c92077b9a23d
carsin <20868514+carsin@users.noreply.github.com>
2020-08-24 23:13:19 -1000

    set up winit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("mine.rs");
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("mine.rs")
+        .with_inner_size(winit::dpi::LogicalSize::new(128.0, 128.0))
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            _ => (),
+        }
+    });
 }
