88a9afbdfbe72f6b06ea2a360cd583d77fb01432
carsin <20868514+carsin@users.noreply.github.com>
2020-09-06 15:05:47 -1000

    revert to crossterm engine

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,76 @@
-extern crate pixels;
-extern crate winit;
-extern crate winit_input_helper;
+extern crate crossterm;
 
-use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
-use winit_input_helper::WinitInputHelper;
+use crossterm::{cursor, style::Print, terminal, QueueableCommand};
+use std::io::{stdout, Write};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
-const BOX_SIZE: i16 = 64;
+mod input;
 
-struct World {
-    box_x: i16,
-    box_y: i16,
-    velocity_x: i16,
-    velocity_y: i16,
-}
+const TPS: u64 = 20;
+const TICK_TIME: Duration = Duration::from_millis(1000 / TPS);
 
-fn main() -> Result<(), Error> {
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new().with_title("Hello Pixels").with_inner_size(size).with_min_inner_size(size).build(&event_loop).unwrap()
-    };
+fn main() {
+    let mut stdout = stdout();
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
+    // Set up terminal
+    stdout.queue(terminal::EnterAlternateScreen).unwrap();
+    stdout.queue(cursor::Hide).unwrap();
+    stdout
+        .queue(terminal::Clear(terminal::ClearType::All))
+        .unwrap();
+    terminal::enable_raw_mode().unwrap();
+    stdout.flush().unwrap();
 
-    let mut world = World::new();
+    let input_receiver = input::get_input();
 
-    event_loop.run(move |event, _, control_flow| {
-        // Draw the current frame
-        if let Event::RedrawRequested(_) = event {
-            world.draw(pixels.get_frame());
-            if pixels.render().map_err(|e| println!("pixels.render() failed: {}", e)).is_err() {
-                *control_flow = ControlFlow::Exit;
-                return;
+    let mut last_time = Instant::now();
+
+    let mut update_count = 0;
+    let mut render_count = 0;
+    let mut sleep_time = Duration::from_millis(0);
+
+    'running: loop {
+        let current_time = Instant::now();
+        let delta_time = current_time.duration_since(last_time);
+
+        last_time = current_time;
+
+        // Handle input
+        while let Ok(char) = input_receiver.try_recv() {
+            match char {
+                'q' => break 'running,
+                _ => (),
             }
         }
+        // Update
+        update_count += 1;
 
-        // Handle input events
-        if input.update(event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
+        // Render
+        stdout
+            .queue(cursor::MoveTo(0, 0))
+            .unwrap()
+            .queue(terminal::Clear(terminal::ClearType::CurrentLine))
+            .unwrap()
+            .queue(Print(format!(
+                "Updates: {:?} Renders: {:?} Delta Time: {:?} Last Sleep Time: {:?}",
+                update_count, render_count, delta_time, sleep_time
+            )))
+            .unwrap();
+        stdout.flush().unwrap();
 
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                pixels.resize(size.width, size.height);
-            }
+        render_count += 1;
 
-            // Update internal state and request a redraw
-            world.update();
-            window.request_redraw();
-        }
-    });
-}
-
-impl World {
-    /// Create a new `World` instance that can draw a moving box.
-    fn new() -> Self {
-        Self {
-            box_x: 24,
-            box_y: 16,
-            velocity_x: 1,
-            velocity_y: 1,
+        if delta_time < TICK_TIME {
+            sleep_time = TICK_TIME - delta_time;
+            sleep(sleep_time);
         }
     }
 
-    /// Update the `World` internal state; bounce the box around the screen.
-    fn update(&mut self) {
-        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
-            self.velocity_x *= -1;
-        }
-
-        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
-            self.velocity_y *= -1;
-        }
-
-        self.box_x += self.velocity_x;
-        self.box_y += self.velocity_y;
-    }
-
-    /// Draw the `World` state to the frame buffer.
-    ///
-    /// Assumes the default texture format: [`wgpu::TextureFormat::Rgba8UnormSrgb`]
-    fn draw(&self, frame: &mut [u8]) {
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % WIDTH as usize) as i16;
-            let y = (i / WIDTH as usize) as i16;
-
-            let inside_the_box = x >= self.box_x
-                && x < self.box_x + BOX_SIZE
-                && y >= self.box_y
-                && y < self.box_y + BOX_SIZE;
-
-            let rgba = if inside_the_box {
-                [0x5e, 0x48, 0xe8, 0xff]
-            } else {
-                [0x48, 0xb2, 0xe8, 0xff]
-            };
-
-            pixel.copy_from_slice(&rgba);
-        }
-    }
+    // Restore terminal after game is finished
+    terminal::disable_raw_mode().unwrap();
+    stdout.queue(cursor::Show).unwrap();
+    stdout.queue(terminal::LeaveAlternateScreen).unwrap();
+    stdout.flush().unwrap();
+    println!("Game exited successfully");
 }
